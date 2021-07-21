956d439c91e69a272290ac646ee3c998e1c775dd
Mohamed Medhat Abdulrahman <Redolitt@gmail.com>
2021-07-22 01:34:38 +0200

    Sudoku Solver code and assets

---
+++ Sudoku_solve.py
@@ -0,0 +1,249 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import os
+
+
+#Custom Functions
+###################################################################################################
+#### READ THE MODEL WEIGHTS
+def intializePredectionModel():
+    model = tf.keras.models.load_model('myModel.h5')
+    #model = load_model('Resources\myModel.h5')
+    return model
+
+
+#### Preprocessing Image
+def preProcess(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # CONVERT IMAGE TO GRAY SCALE
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)  # ADD GAUSSIAN BLUR
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, 1, 1, 11, 2)  # APPLY ADAPTIVE THRESHOLD
+    return imgThreshold
+
+
+####Reorder points for Warp Perspective
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), dtype=np.int32)
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] =myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] =myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+
+
+####FINDING THE BIGGEST COUNTOUR ASSUING THAT IS THE SUDUKO PUZZLE
+def biggestContour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    return biggest,max_area
+
+
+####TO SPLIT THE IMAGE INTO 81 DIFFRENT IMAGES
+def splitBoxes(img):
+    rows = np.vsplit(img,9)
+    boxes=[]
+    for r in rows:
+        cols= np.hsplit(r,9)
+        for box in cols:
+            boxes.append(box)
+    return boxes
+
+
+####GET PREDECTIONS ON ALL IMAGES
+def getPredection(boxes,model):
+    result = []
+    for image in boxes:
+        ## PREPARE IMAGE
+        img = np.asarray(image)
+        img = img[4:img.shape[0] - 4, 4:img.shape[1] -4]
+        img = cv2.resize(img, (28, 28))
+        img = img / 255
+        img = img.reshape(1, 28, 28, 1)
+        ## GET PREDICTION
+        predictions = model.predict(img)
+        classIndex = model.predict_classes(img)
+        probabilityValue = np.amax(predictions)
+        ## SAVE TO RESULT
+        if probabilityValue > 0.8:
+            result.append(classIndex[0])
+        else:
+            result.append(0)
+    return result
+
+
+####TO DISPLAY THE SOLUTION ON THE IMAGE
+def displayNumbers(img,numbers,color = (0,255,0)):
+    secW = int(img.shape[1]/9)
+    secH = int(img.shape[0]/9)
+    for x in range (0,9):
+        for y in range (0,9):
+            if numbers[(y*9)+x] != 0 :
+                 cv2.putText(img, str(numbers[(y*9)+x]),
+                               (x*secW+int(secW/2)-10, int((y+0.8)*secH)), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                            2, color, 2, cv2.LINE_AA)
+    return img
+
+
+####DRAW GRID TO SEE THE WARP PRESPECTIVE EFFICENCY (OPTIONAL)
+def drawGrid(img):
+    secW = int(img.shape[1]/9)
+    secH = int(img.shape[0]/9)
+    for i in range (0,9):
+        pt1 = (0,secH*i)
+        pt2 = (img.shape[1],secH*i)
+        pt3 = (secW * i, 0)
+        pt4 = (secW*i,img.shape[0])
+        cv2.line(img, pt1, pt2, (255, 255, 0),2)
+        cv2.line(img, pt3, pt4, (255, 255, 0),2)
+    return img
+#End of Custom Functions
+############################################################################
+
+#Sudoku Solver algorithm
+############################################################################
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+            if solve(bo):
+                return True
+            bo[row][col] = 0
+    return False
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+    return True
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+    return None
+#End of Soduko Solver
+###################################################################################
+
+
+###################################################################################
+#Main
+heightImg = 450
+widthImg = 450
+model = intializePredectionModel()
+
+#Preparing the image
+img = cv2.imread("sudoku.png")
+img = cv2.resize(img, (widthImg, heightImg))
+imgBlank = np.zeros((heightImg,widthImg, 3),np.uint8)
+imgThreshold = preProcess(img)
+
+#Find All the contours
+imgContours = img.copy()
+imgBigContour = img.copy()
+contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(imgContours, contours, -1 , (0,255,0), 3)
+
+#Find the Biggest Contour to use as Sudoku
+biggest, maxArea = biggestContour(contours) # FIND THE BIGGEST CONTOUR
+print(biggest)
+if biggest.size != 0:
+    biggest = reorder(biggest)
+    print(biggest)
+    cv2.drawContours(imgBigContour, biggest, -1, (0, 0, 255), 25) # DRAW THE BIGGEST CONTOUR
+    pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
+    pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
+    matrix = cv2.getPerspectiveTransform(pts1, pts2) # GER
+    imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+    imgDetectedDigits = imgBlank.copy()
+    imgWarpColored = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
+
+#Split the image and find each digit available
+    imgSolvedDigits = imgBlank.copy()
+    boxes = splitBoxes(imgWarpColored)
+    print(len(boxes))
+    cv2.imshow("Sample",boxes[0])
+    numbers = getPredection(boxes, model)
+    print(numbers)
+    imgDetectedDigits = displayNumbers(imgDetectedDigits, numbers, color=(255, 0, 255))
+    numbers = np.asarray(numbers)
+    posArray = np.where(numbers > 0, 0, 1)
+    print(posArray)
+
+#Find Solutions
+    board = np.array_split(numbers,9)
+    print(board)
+    try:
+        solve(board)
+    except:
+        pass
+    print(board)
+    flatList = []
+    for sublist in board:
+        for item in sublist:
+            flatList.append(item)
+    solvedNumbers =flatList*posArray
+    imgSolvedDigits= displayNumbers(imgSolvedDigits,solvedNumbers)
+
+#OVERLAY SOLUTION
+    pts2 = np.float32(biggest) # PREPARE POINTS FOR WARP
+    pts1 =  np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)  # GER
+    imgInvWarpColored = img.copy()
+    imgInvWarpColored = cv2.warpPerspective(imgSolvedDigits, matrix, (widthImg, heightImg))
+    inv_perspective = cv2.addWeighted(imgInvWarpColored, 1, img, 0.5, 1)
+    imgDetectedDigits = drawGrid(imgDetectedDigits)
+    imgSolvedDigits = drawGrid(imgSolvedDigits)
+    cv2.imshow('Image', img)
+    cv2.imshow('Image Threshold', imgThreshold)
+    cv2.imshow('Contours', imgContours)
+    cv2.imshow("Bigger", imgBigContour)
+    cv2.imshow('detected', imgDetectedDigits)
+    cv2.imshow('Solved Digits', imgSolvedDigits)
+    cv2.imshow('Final', inv_perspective)
+else:
+    print("No Sudoku Found")
+cv2.waitKey(0)
+#End of Main
+###############################################################################
